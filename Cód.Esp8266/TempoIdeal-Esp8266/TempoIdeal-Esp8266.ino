49d1b16fe4c48463cef1923242e31fd9767f36a9
João Gabriel <77994612+Enkiduzis@users.noreply.github.com>
2021-04-20 10:55:56 -0300

    Add files via upload
    Upload das pastas criadas para armazenamento dos documentos criados no projeto.

---
+++ Cód.Esp8266/TempoIdeal-Esp8266/TempoIdeal-Esp8266.ino
@@ -0,0 +1,66 @@
+#include <DHTesp.h>
+#include <ESP8266WiFi.h>
+#include "FirebaseESP8266.h"
+#include <FirebaseESP8266.h>
+ 
+
+#ifdef ESP32
+#pragma message(THIS EXAMPLE IS FOR ESP8266 ONLY!)
+#error Select ESP8266 board.
+#endif
+
+DHTesp dht;
+
+//#define FPM_SLEEP_MAX_TIME           0xFFFFFFF
+#define WIFI_SSID "WALL-E"
+#define WIFI_PASSWORD "Ba0bEc7c9b0f07a608a2ba962$3f903d"
+#define FIREBASE_HOST "tempo-ideal-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "oA3oAkWSmJzCvJMZ8Fs8hTtUlpi0mtUaWD9aEikQ"
+
+FirebaseData fbdo;
+void setup() 
+{
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                  
+  Serial.print("Conectando ao ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+ 
+  Serial.println();
+  Serial.print("Conectado!");
+  Serial.print("Endereço de IP: ");
+  Serial.println(WiFi.localIP()); 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  dht.setup(4, DHTesp::DHT11);
+}
+ 
+void loop() 
+{
+  Serial.print("Estou acordado!");
+  float h = dht.getHumidity();
+  float t = dht.getTemperature();  
+  int s = dht.computeHeatIndex(t, h, false);
+ 
+  Serial.print("Umidade: ");  
+  Serial.print(h);
+  
+  Serial.print("%  Temperatura: ");  
+  Serial.print(t);  
+  Serial.println("°C ");
+
+  Serial.print("Sensação térmica: ");
+  Serial.print(s);
+  Serial.println(".");
+   
+  Firebase.setFloat(fbdo,"Temperatura: ",t);
+  Firebase.setFloat(fbdo,"Umidade: ",h);
+  Firebase.setInt(fbdo,"Sensação Térmica: ",s);
+  Serial.println("deep sleep por 5 minutos, volto já!");
+  wifi_set_sleep_type(NONE_SLEEP_T);
+  delay(300000);
+}
